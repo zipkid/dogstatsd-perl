ebae9befa2afe05eb30bc329243ef42e65fa360e
Fayland Lam <fayland@gmail.com>
2015-01-10 18:07:12 +0800

    fix all stuff

--- README.md
+++ README.md
@@ -9,12 +9,20 @@ DataDog::DogStatsd - A Perl client for DogStatsd
 
         use DataDog::DogStatsd;
 
-        my $stat = DataDog::DogStatsd->new;
-        $stat->increment( 'user.login', { tags => [ $s->cs->param( 'loginName' ) ] } );
+        my $statsd = DataDog::DogStatsd->new;
+        $statsd->increment( 'user.login', { tags => [ $s->cs->param( 'loginName' ) ] } );
+        $statsd->timing('page.load', 320);
+        $statsd->gauge('users.online', 100);
+
+        # namespace version
+        my $statsd = DataDog::DogStatsd->new(namespace => 'account.'); # note the '.' at the ending
+        $statsd->increment('activate'); # actually as account.activate
 
 # DESCRIPTION
 
-DataDog::DogStatsd is a Perl client for DogStatsd
+Statsd: A DogStatsd client (https://www.datadoghq.com)
+
+The difference between [Net::Statsd](https://metacpan.org/pod/Net::Statsd) and this is that it supports tags and namespace.
 
 # METHODS
 
@@ -34,5 +42,52 @@ DataDog::DogStatsd is a Perl client for DogStatsd
 
 ## increment
 
-        $stat->increment( 'user.login' );
-        $stat->increment( 'user.login', { tags => [ $s->cs->param( 'loginName' ) ] } );
+        $statsd->increment( 'page.views' );
+        $statsd->increment( 'test.stats', { sample_rate => 0.5 } );
+        $statsd->increment( 'user.login', { tags => [ $s->cs->param( 'loginName' ) ] } );
+
+Sends an increment (count = 1) for the given stat to the statsd server.
+
+## decrement
+
+        $statsd->decrement( 'products.available' );
+
+Sends a decrement (count = -1) for the given stat to the statsd server.
+
+## count
+
+        $statsd->count( 'test.stats', 2 );
+
+Sends an arbitrary count for the given stat to the statsd server.
+
+## gauge
+
+        $statsd->gauge('users.online', 100);
+        $statsd->gauge('users.online', 100, { tags => ['tag1', 'tag2'] });
+
+Sends an arbitary gauge value for the given stat to the statsd server.
+
+This is useful for recording things like available disk space, memory usage, and the like, which have different semantics than counters.
+
+## histogram
+
+        $statsd->histogram($stats, $value);
+
+Sends a value to be tracked as a histogram to the statsd server.
+
+## timing
+
+        $statsd->timing('page.load', 320);
+        $statsd->timing('page.load', 320, { sample_rate => 0.5, tags => ['tag1', 'tag2'] });
+
+Sends a timing (in ms) for the given stat to the statsd server. The sample\_rate determines what percentage of the time this report is sent. The statsd server then uses the sample\_rate to correctly track the average timing for the stat.
+
+## set
+
+        $statsd->set('visitors.uniques', $user_id);
+
+Sends a value to be tracked as a set to the statsd server.
+
+# AUTHORS
+
+Stefan Goethals <stefan@zipkid.eu>
--- lib/DataDog/DogStatsd.pm
+++ lib/DataDog/DogStatsd.pm
@@ -9,20 +9,6 @@ our $VERSION = '0.01';
 
 use IO::Socket::INET;
 
-# = Statsd: A DogStatsd client (https://www.datadoghq.com)
-#
-# @example Set up a global Statsd client for a server on localhost:8125
-#   require 'statsd'
-#   $statsd = Statsd.new 'localhost', 8125
-# @example Send some stats
-#   $statsd.increment 'page.views'
-#   $statsd.timing 'page.load', 320
-#   $statsd.gauge 'users.online', 100
-# @example Use {#time} to time the execution of a block
-#   $statsd.time('account.activate') { @account.activate! }
-# @example Create a namespaced statsd client and increment 'account.activate'
-#   statsd = Statsd.new('localhost').tap{|sd| sd.namespace = 'account'}
-#   statsd.increment 'activate'
 sub new {
 	my $classname = shift;
 	my $class = ref( $classname ) || $classname;
@@ -38,9 +24,11 @@ sub new {
 sub _socket {
 	my $self = shift;
 	return $self->{_socket} if $self->{_socket};
-	$self->{_socket} = IO::Socket::INET->new( PeerAddr => $self->{'host'},
-											  PeerPort => $self->{'port'},
-											  Proto    => 'udp');
+	$self->{_socket} = IO::Socket::INET->new(
+		PeerAddr => $self->{'host'},
+		PeerPort => $self->{'port'},
+		Proto    => 'udp'
+	);
 	return $self->{_socket};
 }
 
@@ -49,13 +37,6 @@ sub namespace {
 	$self->{'namespace'} = shift;
 }
 
-# Sends an increment (count = 1) for the given stat to the statsd server.
-#
-# @param [String] stat stat name
-# @param [Hash] opts the options to create the metric with
-# @option opts [Numeric] :sample_rate sample rate, 1 for always
-# @option opts [Array<String>] :tags An array of tags
-# @see #count
 sub increment {
 	my $self = shift;
 	my $stat = shift;
@@ -63,98 +44,43 @@ sub increment {
 	$self->count( $stat, 1, $opts );
 }
 
-# Sends a decrement (count = -1) for the given stat to the statsd server.
-#
-# @param [String] stat stat name
-# @param [Hash] opts the options to create the metric with
-# @option opts [Numeric] :sample_rate sample rate, 1 for always
-# @option opts [Array<String>] :tags An array of tags
-# @see #count
 sub decrement {
 	my $self = shift;
 	my $stat = shift;
-  	my $opts = shift || {};
-  	$self->count( $stat, -1, $opts );
+	my $opts = shift || {};
+	$self->count( $stat, -1, $opts );
 }
 
-# Sends an arbitrary count for the given stat to the statsd server.
-#
-# @param [String] stat stat name
-# @param [Integer] count count
-# @param [Hash] opts the options to create the metric with
-# @option opts [Numeric] :sample_rate sample rate, 1 for always
-# @option opts [Array<String>] :tags An array of tags
 sub count {
 	my $self  = shift;
 	my $stat  = shift;
-  	my $count = shift;
-  	my $opts  = shift || {};
-  	$self->send_stats( $stat, $count, 'c', $opts );
+	my $count = shift;
+	my $opts  = shift || {};
+	$self->send_stats( $stat, $count, 'c', $opts );
 }
 
-# Sends an arbitary gauge value for the given stat to the statsd server.
-#
-# This is useful for recording things like available disk space,
-# memory usage, and the like, which have different semantics than
-# counters.
-#
-# @param [String] stat stat name.
-# @param [Numeric] gauge value.
-# @param [Hash] opts the options to create the metric with
-# @option opts [Numeric] :sample_rate sample rate, 1 for always
-# @option opts [Array<String>] :tags An array of tags
-# @example Report the current user count:
-#   $statsd.gauge('user.count', User.count)
-##def gauge(stat, value, opts={})
-##	send_stats stat, value, :g, opts
-##end
 sub gauge {
 	my $self  = shift;
 	my $stat  = shift;
-  	my $value = shift;
-  	my $opts  = shift || {};
-  	$self->send_stats( $stat, $value, 'g', $opts );
+	my $value = shift;
+	my $opts  = shift || {};
+	$self->send_stats( $stat, $value, 'g', $opts );
 }
 
-# Sends a value to be tracked as a histogram to the statsd server.
-#
-# @param [String] stat stat name.
-# @param [Numeric] histogram value.
-# @param [Hash] opts the options to create the metric with
-# @option opts [Numeric] :sample_rate sample rate, 1 for always
-# @option opts [Array<String>] :tags An array of tags
-# @example Report the current user count:
-#   $statsd.histogram('user.count', User.count)
-##def histogram(stat, value, opts={})
-##	send_stats stat, value, :h, opts
-##end
 sub histogram {
 	my $self  = shift;
 	my $stat  = shift;
-  	my $value = shift;
-  	my $opts  = shift || {};
-  	$self->send_stats( $stat, $value, 'h', $opts );
+	my $value = shift;
+	my $opts  = shift || {};
+	$self->send_stats( $stat, $value, 'h', $opts );
 }
 
-# Sends a timing (in ms) for the given stat to the statsd server. The
-# sample_rate determines what percentage of the time this report is sent. The
-# statsd server then uses the sample_rate to correctly track the average
-# timing for the stat.
-#
-# @param [String] stat stat name
-# @param [Integer] ms timing in milliseconds
-# @param [Hash] opts the options to create the metric with
-# @option opts [Numeric] :sample_rate sample rate, 1 for always
-# @option opts [Array<String>] :tags An array of tags
-##def timing(stat, ms, opts={})
-##	send_stats stat, ms, :ms, opts
-##end
 sub timing {
 	my $self = shift;
 	my $stat = shift;
-  	my $ms   = shift;
-  	my $opts = shift || {};
-  	$self->send_stats( $stat, $ms, 'ms', $opts );
+	my $ms   = shift;
+	my $opts = shift || {};
+	$self->send_stats( $stat, $ms, 'ms', $opts );
 }
 
 # Reports execution time of the provided block using {#timing}.
@@ -174,35 +100,23 @@ sub timing {
 ##	result
 ##end
 
-# Sends a value to be tracked as a set to the statsd server.
-#
-# @param [String] stat stat name.
-# @param [Numeric] set value.
-# @param [Hash] opts the options to create the metric with
-# @option opts [Numeric] :sample_rate sample rate, 1 for always
-# @option opts [Array<String>] :tags An array of tags
-# @example Record a unique visitory by id:
-#   $statsd.set('visitors.uniques', User.id)
-##def set(stat, value, opts={})
-##	send_stats stat, value, :s, opts
-##end
 sub set {
 	my $self  = shift;
 	my $stat  = shift;
-  	my $value = shift;
-  	my $opts  = shift || {};
-  	$self->send_stats( $stat, $value, 's', $opts );
+	my $value = shift;
+	my $opts  = shift || {};
+	$self->send_stats( $stat, $value, 's', $opts );
 }
 
 sub send_stats {
 	my $self  = shift;
 	my $stat  = shift;
-  	my $delta = shift;
-  	my $type  = shift;
-  	my $opts  = shift || {};
+	my $delta = shift;
+	my $type  = shift;
+	my $opts  = shift || {};
 
-  	my $sample_rate = defined $opts->{'sample_rate'} ? $opts->{'sample_rate'} : 1;
-  	if( $sample_rate == 1 || rand() <= $sample_rate ) {
+	my $sample_rate = defined $opts->{'sample_rate'} ? $opts->{'sample_rate'} : 1;
+	if( $sample_rate == 1 || rand() <= $sample_rate ) {
 		$stat =~ s/::/./g;
 		$stat =~ s/[:|@]/_/g;
 		my $rate = '';
@@ -233,14 +147,22 @@ DataDog::DogStatsd - A Perl client for DogStatsd
 
 =head1 SYNOPSIS
 
-  	use DataDog::DogStatsd;
+	use DataDog::DogStatsd;
 
-  	my $stat = DataDog::DogStatsd->new;
-	$stat->increment( 'user.login', { tags => [ $s->cs->param( 'loginName' ) ] } );
+	my $statsd = DataDog::DogStatsd->new;
+	$statsd->increment( 'user.login', { tags => [ $s->cs->param( 'loginName' ) ] } );
+	$statsd->timing('page.load', 320);
+	$statsd->gauge('users.online', 100);
+
+	# namespace version
+	my $statsd = DataDog::DogStatsd->new(namespace => 'account.'); # note the '.' at the ending
+	$statsd->increment('activate'); # actually as account.activate
 
 =head1 DESCRIPTION
 
-DataDog::DogStatsd is a Perl client for DogStatsd
+Statsd: A DogStatsd client (https://www.datadoghq.com)
+
+The difference between L<Net::Statsd> and this is that it supports tags and namespace.
 
 =head1 METHODS
 
@@ -264,5 +186,52 @@ default to ''
 
 =head2 increment
 
-	$stat->increment( 'user.login' );
-	$stat->increment( 'user.login', { tags => [ $s->cs->param( 'loginName' ) ] } );
+	$statsd->increment( 'page.views' );
+	$statsd->increment( 'test.stats', { sample_rate => 0.5 } );
+	$statsd->increment( 'user.login', { tags => [ $s->cs->param( 'loginName' ) ] } );
+
+Sends an increment (count = 1) for the given stat to the statsd server.
+
+=head2 decrement
+
+	$statsd->decrement( 'products.available' );
+
+Sends a decrement (count = -1) for the given stat to the statsd server.
+
+=head2 count
+
+	$statsd->count( 'test.stats', 2 );
+
+Sends an arbitrary count for the given stat to the statsd server.
+
+=head2 gauge
+
+	$statsd->gauge('users.online', 100);
+	$statsd->gauge('users.online', 100, { tags => ['tag1', 'tag2'] });
+
+Sends an arbitary gauge value for the given stat to the statsd server.
+
+This is useful for recording things like available disk space, memory usage, and the like, which have different semantics than counters.
+
+=head2 histogram
+
+	$statsd->histogram($stats, $value);
+
+Sends a value to be tracked as a histogram to the statsd server.
+
+=head2 timing
+
+	$statsd->timing('page.load', 320);
+	$statsd->timing('page.load', 320, { sample_rate => 0.5, tags => ['tag1', 'tag2'] });
+
+Sends a timing (in ms) for the given stat to the statsd server. The sample_rate determines what percentage of the time this report is sent. The statsd server then uses the sample_rate to correctly track the average timing for the stat.
+
+=head2 set
+
+	$statsd->set('visitors.uniques', $user_id);
+
+Sends a value to be tracked as a set to the statsd server.
+
+=head1 AUTHORS
+
+Stefan Goethals <stefan@zipkid.eu>
